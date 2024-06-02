07aafa074b80acc93b3d366cd3584bfa07bfb8fe
kimJongWan <kimdeiz@naver.com>
2024-06-02 21:32:12 +0900

    spring security 초기 설정 및 h2-console 사용 가능하도록 설정

---
+++ src/main/kotlin/com/together_english/deiz/security/WebSecurityConfig.kt
@@ -0,0 +1,40 @@
+package com.together_english.deiz.security
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
+import org.springframework.security.config.annotation.web.invoke
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
+@Configuration
+@EnableWebSecurity
+class WebSecurityConfig {
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http {
+            authorizeHttpRequests {
+                authorize("/", permitAll)
+                authorize("/h2-console/**", permitAll) // H2 콘솔 경로에 대한 접근 허용
+            }
+            formLogin { }
+            httpBasic { }
+            headers {
+                frameOptions {
+                    sameOrigin = true // H2 콘솔이 iframe 내에서 올바르게 로드되도록 설정
+                }
+            }
+        }
+        return http.build()
+    }
+
+    @Bean
+    fun webSecurityCustomizer() = WebSecurityCustomizer { web ->
+        web.ignoring().requestMatchers(AntPathRequestMatcher("/h2-console/**"))
+    }
+
+
+}
---
+++ src/test/kotlin/com/together_english/deiz/controller/HelloControllerTest.kt
@@ -0,0 +1,21 @@
+package com.together_english.deiz.controller
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HelloControllerTest(@Autowired private val mockMvc: MockMvc) {
+    @Test
+    fun `index page is not protected`() {
+        this.mockMvc
+                .get("/")
+                .andExpect {
+                    status { isOk() }
+                }
+    }
+}
