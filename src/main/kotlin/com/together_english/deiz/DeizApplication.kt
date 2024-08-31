ce664dacfb9dd1ec913530a6bed2b5f78cbc10f3
kimJongWan <kimdeiz@naver.com>
2024-08-31 18:33:43 +0900

    생성일자 및 업데이트날짜가 데이터 insert시 자동으로 포함될 수 있도록 @EnableJpaAuditing 어노테이션 추가

--- src/main/kotlin/com/together_english/deiz/DeizApplication.kt
+++ src/main/kotlin/com/together_english/deiz/DeizApplication.kt
@@ -2,8 +2,10 @@ package com.together_english.deiz
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class DeizApplication
 
 fun main(args: Array<String>) {
