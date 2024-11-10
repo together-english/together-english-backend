d66939b65519950f447e5d7ef78965ed00ba8d2d
kodh1000 <kodh10@gmail.com>
2024-11-11 01:23:25 +0900

    Comment 조회(페이지네이션) 반영

--- src/main/kotlin/com/together_english/deiz/model/circle/CircleComment.kt
+++ src/main/kotlin/com/together_english/deiz/model/circle/CircleComment.kt
@@ -9,9 +9,9 @@ import java.util.*
 
 @Entity
 class CircleComment(
-    content : String,
-    circle : Circle,
-    member : Member,
+    content: String,
+    circle: Circle,
+    member: Member,
 ) : BaseTimeEntity() {
 
     @Id
@@ -20,23 +20,23 @@ class CircleComment(
     @Column(length = 255)
     @NotNull
     var content: String = content
-    private set
+        private set
 
     @Column(length = 50)
     @Enumerated(value = EnumType.STRING)
     @NotNull
     var status: CommentStatus = CommentStatus.REPORTED
-    private set
+        private set
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "circle_id")
     @NotNull
-    val circle : Circle = circle
+    val circle: Circle = circle
 
     @ManyToOne(fetch = FetchType.EAGER, optional = false)
     @JoinColumn(name = "member_id")
     @NotNull
-    val member : Member = member
+    val member: Member = member
 
     fun isWrittenBy(member: Member): Boolean {
         return this.member.id == member.id
