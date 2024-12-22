a120c2b0cbda9def52469280e1a14bd529d9f062
kodh1000 <kodh10@gmail.com>
2024-12-22 15:04:37 +0900

    모임 가입한 유저 정보 조회, 내 가입모임 조회, 일반 모임 목록 조회 좋아요 관련 쿼리 수정

--- src/main/kotlin/com/together_english/deiz/repository/CircleMemberRepository.kt
+++ src/main/kotlin/com/together_english/deiz/repository/CircleMemberRepository.kt
@@ -3,9 +3,11 @@ package com.together_english.deiz.repository
 import com.together_english.deiz.model.circle.Circle
 import com.together_english.deiz.model.circle.CircleMember
 import com.together_english.deiz.model.member.entity.Member
+import com.together_english.deiz.repository.custom.CustomCircleMemberRepository
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface CircleMemberRepository: JpaRepository<CircleMember, UUID> {
+interface CircleMemberRepository: JpaRepository<CircleMember, UUID>, CustomCircleMemberRepository {
     fun findByCircleAndMember(circle: Circle, member: Member): Optional<CircleMember>
+    fun existsByCircleIdAndMemberId(circleId: UUID, memberId: UUID): Boolean
 }
--- src/main/kotlin/com/together_english/deiz/security/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/together_english/deiz/security/config/WebSecurityConfig.kt
@@ -31,6 +31,7 @@ class WebSecurityConfig(
                 authorize("/auth/**", permitAll)
                 authorize("/member/**", hasAuthority(Role.USER.authority))
 
+                authorize(HttpMethod.GET, "/circle/{circleId}/member", hasAuthority(Role.USER.authority))
                 authorize(HttpMethod.GET, "/circle/**", permitAll)
                 authorize(HttpMethod.POST,"/circle/list", permitAll)
                 authorize(HttpMethod.POST,"/circle/**", hasAuthority(Role.USER.authority))
