a0c7a729a83e3f5ace9466357ab17b9810a446cd
kodh1000 <kodh10@gmail.com>
2025-02-09 12:50:42 +0900

    [채팅구현] 요약 : MongoDB UUID 관련 설정

--- src/main/kotlin/com/together_english/deiz/chat/config/MongoConfig.kt
+++ src/main/kotlin/com/together_english/deiz/chat/config/MongoConfig.kt
@@ -1,7 +1,10 @@
 package com.together_english.deiz.chat.config
 
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import org.bson.UuidRepresentation
 import org.springframework.boot.autoconfigure.mongo.MongoProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -15,11 +18,25 @@ class MongoConfig(
 ) {
     @Bean
     fun mongoClient(): MongoClient {
-        return MongoClients.create(mongoProperties.uri)
+        val connectionString = ConnectionString(mongoProperties.uri)
+
+        val mongoClientSettings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .uuidRepresentation(UuidRepresentation.STANDARD)
+            .build()
+        return MongoClients.create(mongoClientSettings)
     }
 
     @Bean
     fun mongoTemplate(): MongoTemplate {
         return MongoTemplate(mongoClient(), mongoProperties.database)
+    }
+
+    @Bean
+    fun mongo(): MongoClient {
+        val mongoClientSettings = MongoClientSettings.builder()
+            .uuidRepresentation(UuidRepresentation.STANDARD)
+            .build()
+        return MongoClients.create(mongoClientSettings)
     }
 }
